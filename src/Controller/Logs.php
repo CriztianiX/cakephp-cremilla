e4b174d5885e831a42badb0b20df3f5144ae2f8e
Criztian Haunsen <cristian@mundodescuento.com>
2017-02-16 05:35:09 +0000

    Skeleton for controller

---
+++ src/Controller/Logs.php
@@ -0,0 +1,18 @@
+<?php
+namespace CriztianiX\Cremilla\Controller;
+
+use App\Controller\AppController;
+
+
+class LogsController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+
+    }
+}
